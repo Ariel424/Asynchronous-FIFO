03aca11c16d25fdb9ea34b6b8166601eff8dd45c
Ariel Topaz <130974714+Ariel424@users.noreply.github.com>
2026-05-19 16:07:33 +0300

    Update Interface.sv

--- Interface.sv
+++ Interface.sv
@@ -1,29 +1,28 @@
 interface my_interface (input logic wclk, rclk);
-    
-logic wreset;
-logic rreset;
-logic write;
-logic read;
-logic full;
-logic empty;
-logic [7:0] data_in, data_out;
 
-clocking w_cb @(posedge wclk);
-    default input #1ns output #1ns;
-    output write;
-    output data_in;
-    input  full;
-endclocking
+    logic wreset, rreset;
+    logic write, read;
+    logic full, empty;
+    logic [7:0] data_in, data_out;
 
-clocking r_cb @(posedge rclk);
-    default input #1ns output #1ns;
-    output read;
-    input  data_out;
-    input  empty;
- endclocking
+    clocking w_cb @(posedge wclk);
+        default input #1ns output #1ns;
+        output write;
+        output data_in;
+        input  full;
+    endclocking
 
-modport DRIVER_MP (clocking w_cb, clocking r_cb, output wreset, output rreset);
-modport W_MONITOR_MP (clocking w_cb, input wreset);
-modport R_MONITOR_MP (clocking r_cb, input rreset);
-    
-endinterface 
+    clocking r_cb @(posedge rclk);
+        default input #1ns output #1ns;
+        output read;
+        input  data_out;
+        input  empty;
+    endclocking
+
+    modport W_DRIVER_MP  (clocking w_cb, output wreset);
+    modport W_MONITOR_MP (clocking w_cb, input wreset);
+
+    modport R_DRIVER_MP  (clocking r_cb, output rreset);
+    modport R_MONITOR_MP (clocking r_cb, input rreset);
+
+endinterface
